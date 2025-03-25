0e83317bd53e307c19d2e242d4fce0fd667f507c
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2025-03-25 09:48:33 -0300

    initalizing..

---
+++ src/test/java/org/guilherme/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guilherme.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
