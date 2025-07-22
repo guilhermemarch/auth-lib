d6eda72243324a9915012bb07be20dc588d3ece2
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2025-07-22 11:42:51 -0300

    fix: email messages and some exceptions translated to portuguese

--- src/main/java/org/guilherme/authapi/service/CustomUserDetailsService.java
+++ src/main/java/org/guilherme/authapi/service/CustomUserDetailsService.java
@@ -19,7 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o nome: " + username));
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
